663b7932e7b1c57c9c6d3ed92111b5a90ce3638f
smuhammmadaziz <sayhelloaziz@gmail.com>
2025-04-16 10:21:58 +0500

    bump version to 2.0.0 and update display in App component; add new heading in App

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ function App() {
     <>
       <UpdateNotification />
 
-      <div>v1.0.2</div>
+      <div>v2.0.0</div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero
+        perferendis eligendi accusamus veniam dicta rerum, nobis fugiat tempore
+        atque dolorum qui vero autem voluptatem ut! Inventore exercitationem
+        amet aspernatur beatae?
+      </h1>
     </>
   );
 }
